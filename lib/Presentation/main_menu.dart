fe57743a4b3ec9bc200b09e56276ab7014e1a291
vivu19 <vivu19@student.sdu.dk>
2024-02-27 14:42:53 +0100

    Navbar is now persistent in all views

--- lib/Presentation/main_menu.dart
+++ lib/Presentation/main_menu.dart
@@ -3,6 +3,7 @@ import 'package:compassion_app/Presentation/exercises.dart';
 import 'package:compassion_app/Presentation/knowledge.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 import './Components/constants.dart';
 import './Components/videoPlayer.dart';
 import './home.dart';
@@ -15,47 +16,84 @@ class Main_menu extends StatefulWidget {
 }
 
 class _Main_menuState extends State<Main_menu> {
-  int _currentIndex = 0;
+  var activeColor = Constants.sduGoldColor;
+  var unSelectedColor = Constants.kBlackColor;
 
-  final List<Widget> _pages = [
-    Home(),
-    Training(),
-    Knowledge(),
-  ];
+  late PersistentTabController _controller;
+  late bool _hideNavBar;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PersistentTabController(initialIndex: 0);
+    _hideNavBar = false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_currentIndex], // Display the current page
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        selectedItemColor: Constants.sduGoldColor,
-        unselectedItemColor: Constants.kBlackColor,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        iconSize: 30,
-        selectedFontSize: 15,
-        backgroundColor: Constants.kWhiteColor,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.home),
-            label: 'Hjem',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.briefcase),
-            label: 'Træning',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.book),
-            label: 'Viden',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.library_music),
-            label: 'JustAClass',
-          ),
-        ],
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
       ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.style3, // Choose the nav bar style with this property.
     );
+  }
+
+  List<Widget> _buildScreens() {
+    return [
+      Home(),
+      Training(),
+      Knowledge(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.home),
+        title: "Hjem",
+        activeColorPrimary: activeColor,
+        inactiveColorPrimary: unSelectedColor,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.briefcase),
+        title: "Træning",
+        activeColorPrimary: activeColor,
+        inactiveColorPrimary: unSelectedColor,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.book),
+        title: "Viden",
+        activeColorPrimary: activeColor,
+        inactiveColorPrimary: unSelectedColor,
+      ),
+    ];
   }
 }
