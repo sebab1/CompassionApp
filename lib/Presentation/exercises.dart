b2d054c0889e10f23df122f8f9665d28889a933b
vivu19 <vivu19@student.sdu.dk>
2024-03-20 14:23:19 +0100

    minor fix

--- lib/Presentation/exercises.dart
+++ lib/Presentation/exercises.dart
@@ -125,7 +125,7 @@ class Exercises extends StatelessWidget {
                                         title: card.title,
                                         desc: card.desc,
                                         audio: card.audioPath,
-                                        isMeditation: false,
+                                        category: 'Øvelser',
                                       )),
                             );
                           },
