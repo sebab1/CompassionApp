153e8a8dd79e6a8bcc1a5427a2b343510b15a097
vivu19 <vivu19@student.sdu.dk>
2024-04-22 15:38:05 +0200

    Now saves previous state for notification settings

--- lib/Presentation/Components/notification_settings.dart
+++ lib/Presentation/Components/notification_settings.dart
@@ -2,6 +2,7 @@ import 'package:compassion_app/Domain/notification_api.dart';
 import 'package:compassion_app/Presentation/Components/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wheel_picker/wheel_picker.dart';
 
 class NotificationSettings extends StatefulWidget {
@@ -13,26 +14,60 @@ class NotificationSettings extends StatefulWidget {
 }
 
 class _NotificationSettingsState extends State<NotificationSettings> {
-
+  late SharedPreferences sharedPreferences;
 
   bool switchOn = false;
   late NotificationApi notificationApi;
 
   final now = TimeOfDay.now();
   late final hoursWheel = WheelPickerController(
-    itemCount: 13,
-    initialIndex: now.hour % 12,
+    itemCount: 24,
+    initialIndex: setWheelPickerState('hours') ?? now.hour % 12,
   );
   late final minutesWheel = WheelPickerController(
     itemCount: 60,
-    initialIndex: now.minute,
+    initialIndex: setWheelPickerState('minutes') ?? now.minute,
   );
+
+  Future updateWheelPickerState(int hours, int minutes) {
+    return Future.wait([
+      sharedPreferences.setInt('hours', hours),
+      sharedPreferences.setInt('minutes', minutes)
+    ]);
+  }
+
+  Future init() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  bool? switchState() {
+    return sharedPreferences.getBool('switchState');
+  }
+
+  int? setWheelPickerState(String time) {
+    return sharedPreferences.getInt(time);
+  }
+
+  Future updateSwitchState(bool value) {
+    return sharedPreferences.setBool('switchState', value);
+  }
 
   @override
   void initState() {
-    // TODO: implement initState
+    init().then((value) => setState(() {
+          switchOn = switchState() ?? false;
+        }));
+
     notificationApi = widget.notificationApi;
   }
+
+  @override
+  void dispose() {
+    hoursWheel.dispose();
+    minutesWheel.dispose();
+    super.dispose();
+  }
+
   showSnackBar(context) {
     SnackBar snackBar = SnackBar(
       content: const Text('Din daglig påmindelse er gemt',
@@ -48,6 +83,7 @@ class _NotificationSettingsState extends State<NotificationSettings> {
 
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
+
   @override
   Widget build(BuildContext context) {
     const textStyle = TextStyle(fontSize: 26.0, height: 1.5);
@@ -63,8 +99,6 @@ class _NotificationSettingsState extends State<NotificationSettings> {
     Widget itemBuilder(BuildContext context, int index) {
       return Text("$index".padLeft(2, '0'), style: textStyle);
     }
-
-
 
     return Column(
       children: [
@@ -83,15 +117,18 @@ class _NotificationSettingsState extends State<NotificationSettings> {
                 Icon(switchOn
                     ? Icons.notifications_active
                     : Icons.notifications),
-                    Container(padding: EdgeInsets.fromLTRB(4, 0, 0, 0),),
+                Container(
+                  padding: EdgeInsets.fromLTRB(4, 0, 0, 0),
+                ),
                 Switch(
                     activeColor: Constants.sduGoldColor,
                     value: switchOn,
                     onChanged: (bool value) async {
-                      if(!value){
+                      if (!value) {
                         print('cancel notification');
                         await notificationApi.cancelAllNotification();
                       }
+                      updateSwitchState(value);
                       setState(() {
                         switchOn = value;
                       });
@@ -126,15 +163,25 @@ class _NotificationSettingsState extends State<NotificationSettings> {
               ),
               ElevatedButton(
                 style: const ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll<Color>(Constants.sduGoldColor)),
-                onPressed: (
-                ) async {
-
-                  await notificationApi.recurringNotification(title: 'Compassion App', body: 'Måske trænger du til tid til afstresning med en at skrive i din dagbog eller meditation.', payload: '123.ab', hour: hoursWheel.selected, minutes: minutesWheel.selected).
-                  then((value) => showSnackBar(context));
-
+                    backgroundColor: MaterialStatePropertyAll<Color>(
+                        Constants.sduGoldColor)),
+                onPressed: () async {
+                  int hours = hoursWheel.selected;
+                  int minutes = minutesWheel.selected;
+                  await notificationApi
+                      .recurringNotification(
+                          title: 'Daglig påmindelse',
+                          body:
+                              'Trænger du til at afstresse med meditation eller at skrive et dagbogsnotat?',
+                          payload: '123.ab',
+                          hour: hours,
+                          minutes: minutes)
+                      .then((value) => updateWheelPickerState(hours, minutes).then((value) => showSnackBar(context)));
                 },
-                child: Text('Gem', style: TextStyle(color: Constants.kBlackColor),),
+                child: Text(
+                  'Gem',
+                  style: TextStyle(color: Constants.kBlackColor),
+                ),
               )
             ],
           ),
