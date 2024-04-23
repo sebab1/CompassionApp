90598ec02875177d80c2ad588eafb3552ebfe9e1
vivu19 <vivu19@student.sdu.dk>
2024-04-23 13:46:34 +0200

    Upgrading db should now work

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -16,17 +16,15 @@ final sl = GetIt.instance;
 
 final String db_name = 'sqlite-db';
 
-final expected_db_version = 2;
+final expectedDBVersion = 2;
 
 Future<void> init() async {
   final Database database = await _initDatabase();
   sl.registerLazySingleton<Database>(() => database);
 
-  await editDatabase(database);
-
   var sql = SqlDatabase(sl());
 
-  // await sql.deleteDatabase(await getDatabasesPath());
+  //await sql.deleteDatabase(await getDatabasesPath());
   sl.registerLazySingleton<IScraperController>(() => ScraperController());
 
   sl.registerLazySingleton<KollegaCafe>(() => KollegaCafe(sc: sl()));
@@ -46,44 +44,56 @@ Future<void> init() async {
 
 Future<Database> _initDatabase() async {
   String path = await getDatabasesPath();
-  return await openDatabase(
-    join(path, db_name),
-    onCreate: (db, version) {
-      print('creating tables');
-      String entriesTable =
-          'CREATE TABLE IF NOT EXISTS entries(entry_id INTEGER PRIMARY KEY AUTOINCREMENT, date INTEGER NOT NULL)';
-      String activityJournalTable =
-          'CREATE TABLE IF NOT EXISTS activityJournal(activity_id INTEGER PRIMARY KEY AUTOINCREMENT, entry_id INTEGER NOT NULL UNIQUE, activity_desc TEXT NOT NULL, FOREIGN KEY (entry_id) REFERENCES entries)';
-      String intentionTable =
-          'CREATE TABLE IF NOT EXISTS intentionJournal(intention_id INTEGER PRIMARY KEY AUTOINCREMENT, entry_id INTEGER NOT NULL UNIQUE, intention_desc TEXT NOT NULL, FOREIGN KEY (entry_id) REFERENCES entries)';
-
-      db.execute(entriesTable);
-      db.execute(activityJournalTable);
-      db.execute(intentionTable);
-    },
-    version: 1,
-  );
+  return await openDatabase(join(path, db_name), version: expectedDBVersion,
+      onUpgrade: (Database db, int oldVersion, int newVersion) async {
+    //
+    //Iterate from the current version to the latest version and execute SQL statements
+    for (int version = oldVersion; version < newVersion; version++) {
+      await _performDBUpgrade(db, version + 1);
+    }
+  }, onCreate: (Database db, int newVersion) async {
+    //
+    //Start from version 1 to current version and create DB
+    for (int version = 1; version < newVersion; version++) {
+      await _performDBUpgrade(db, version + 1);
+    }
+  });
 }
 
-Future editDatabase(Database db) async {
-  int version = await db.getVersion();
-  print('db version: $version');
-  var future;
-  if (version < expected_db_version) {
-    var update = db
-        .execute(
-            'ALTER TABLE intentionJournal ADD COLUMN checked INTEGER NOT NULL DEFAULT 0; ')
-        .then((value) => db.setVersion(2));
-    print('updating db');
-    future = update;
-  }
-  return future;
-}
+Future<void> _performDBUpgrade(Database db, int upgradeToVersion) async {
+  print(upgradeToVersion);
+  switch (upgradeToVersion) {
+    //Upgrade to V1 (initial creation)
+    case 1:
+      await _dbUpdatesVersion_1(db);
+      break;
 
-void _onUpgrade(Database db, int oldVersion, int newVersion) {
-  if (oldVersion < newVersion) {
-    // you can execute drop table and create table
-    db.execute(
-        'ALTER TABLE intentionJournal ADD COLUMN checked INTEGER NOT NULL DEFAULT 0;');
+    //Upgrades for V2
+    case 2:
+      await _dbUpdatesVersion_2(db);
+      break;
   }
 }
+
+Future<void> _dbUpdatesVersion_1(Database db) async {
+  print('creating tables');
+  String entriesTable =
+      'CREATE TABLE IF NOT EXISTS entries(entry_id INTEGER PRIMARY KEY AUTOINCREMENT, date INTEGER NOT NULL)';
+  String activityJournalTable =
+      'CREATE TABLE IF NOT EXISTS activityJournal(activity_id INTEGER PRIMARY KEY AUTOINCREMENT, entry_id INTEGER NOT NULL UNIQUE, activity_desc TEXT NOT NULL, FOREIGN KEY (entry_id) REFERENCES entries)';
+  String intentionTable =
+      'CREATE TABLE IF NOT EXISTS intentionJournal(intention_id INTEGER PRIMARY KEY AUTOINCREMENT, entry_id INTEGER NOT NULL UNIQUE, intention_desc TEXT NOT NULL, FOREIGN KEY (entry_id) REFERENCES entries)';
+
+  await db.execute(entriesTable);
+  await db.execute(activityJournalTable);
+  await db.execute(intentionTable);
+}
+
+Future<void> _dbUpdatesVersion_2(Database db) async {
+  print('adding new column');
+  await db.execute(
+      'ALTER TABLE intentionJournal ADD COLUMN checked INTEGER NOT NULL DEFAULT 0;');
+}
+
+Future<void> deleteDatabase(String path) =>
+    databaseFactory.deleteDatabase(path);
